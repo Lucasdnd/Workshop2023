155cdb1c7eb1476f7338e5f30b925db4a31a3c53
Lucas Durand <47317956+Lucasdnd@users.noreply.github.com>
2023-03-30 17:08:38 +0200

    resolution conflits v2

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -13,33 +13,6 @@
             <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}" data-widget="treeview" role="menu" @if(config('adminlte.sidebar_nav_animation_speed') !=300) data-animation-speed="{{ config('adminlte.sidebar_nav_animation_speed') }}" @endif @if(!config('adminlte.sidebar_nav_accordion')) data-accordion="false" @endif>
                 {{-- Configured sidebar links --}}
                 @each('adminlte::partials.sidebar.menu-item', $adminlte->menu('sidebar'), 'item')
-                <li class="nav-item">
-                    <a href="{{ route('contact.leads') }}" class="nav-link">
-                        <i class="fas fa-users"></i>
-                        <p>Leads</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('contact.prospects') }}" class="nav-link">
-                        <i class="fas fa-user"></i>
-                        <p>Prospects</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('contact.clients') }}" class="nav-link">
-                        <i class="fas fa-user-check"></i>
-                        <p>Clients</p>
-                    </a>
-                </li>
-            </ul>
-            <hr class="mt-0">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item">
-                    <a href="{{ route('users.index') }}" class="nav-link">
-                        <i class="fas fa-users-cog"></i>
-                        <p>Gestion des utilisateurs</p>
-                    </a>
-                </li>
             </ul>
         </nav>
     </div>
